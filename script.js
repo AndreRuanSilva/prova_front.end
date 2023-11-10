49ec1c3911b0b3e655577880c91ba56241da74c3
rersand <134522744+AndreRuanSilva@users.noreply.github.com>
2023-11-10 12:30:44 -0300

    button mensgem

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function mudarTexto() {
     document.getElementById("demo").innerHTML = "nota maxima?";
 }
 alert("hello word");
+
+function mensagem(){
+    document.querySelector('#TocaMensagem').play();
+}
+document.querySelector('TocaMensagem').onclick = mensagem;
